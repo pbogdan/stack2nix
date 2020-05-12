aa2bcacedfd685402e778f63657d81db81278993
Piotr Bogdan <ppbogdan@gmail.com>
2020-05-12 14:51:37 +0100

    Clean up nix variable scoping, make lib profiling optional

--- default.nix
+++ default.nix
@@ -1,35 +1,54 @@
-{ pkgs ? import (import ./fetch-nixpkgs.nix) {} }:
-
-with pkgs.haskell.lib;
-
+{ pkgs ? import (import ./fetch-nixpkgs.nix) {}
+, profiling ? false
+}:
+let
+  inherit (pkgs.lib)
+    optionalAttrs
+    ;
+  inherit (pkgs.haskell.lib)
+    disableCabalFlag
+    doJailbreak
+    justStaticExecutables
+    overrideCabal
+    ;
+in
 (
   (import ./stack2nix.nix { inherit pkgs; }).override {
-    overrides = self: super: {
-      # TODO: separate out output
-      stack2nix = justStaticExecutables (
-        overrideCabal super.stack2nix (
-          old: {
-            src = builtins.path {
-              name = "stack2nix";
-              path = ./.;
-              # Filter hidden dirs (.), e.g. .git and .stack-work
-              # TODO Remove once https://github.com/input-output-hk/stack2nix/issues/119 is done
-              filter = path: type:
-                !(pkgs.lib.hasPrefix "." (baseNameOf path))
-                && baseNameOf path != "stack.yaml";
-            };
+    overrides = self: super: (
+      {
+
+        # TODO: separate out output
+        stack2nix = justStaticExecutables (
+          overrideCabal super.stack2nix (
+            old: {
+              src = builtins.path {
+                name = "stack2nix";
+                path = ./.;
+                # Filter hidden dirs (.), e.g. .git and .stack-work
+                # TODO Remove once https://github.com/input-output-hk/stack2nix/issues/119 is done
+                filter = path: type:
+                  !(pkgs.lib.hasPrefix "." (baseNameOf path))
+                  && baseNameOf path != "stack.yaml";
+              };
+            }
+          )
+        );
+
+        # https://github.com/commercialhaskell/lts-haskell/issues/149
+        stack = doJailbreak super.stack;
+
+        # needed until we upgrade to 18.09
+        yaml = disableCabalFlag super.yaml "system-libyaml";
+
+        # https://github.com/NixOS/cabal2nix/issues/146
+        hinotify = if pkgs.stdenv.isDarwin then self.hfsevents else super.hinotify;
+      } // optionalAttrs (!profiling) {
+        mkDerivation = args: super.mkDerivation (
+          args // {
+            enableLibraryProfiling = false;
           }
-        )
-      );
-
-      # https://github.com/commercialhaskell/lts-haskell/issues/149
-      stack = doJailbreak super.stack;
-
-      # needed until we upgrade to 18.09
-      yaml = disableCabalFlag super.yaml "system-libyaml";
-
-      # https://github.com/NixOS/cabal2nix/issues/146
-      hinotify = if pkgs.stdenv.isDarwin then self.hfsevents else super.hinotify;
-    };
+        );
+      }
+    );
   }
 ).stack2nix
